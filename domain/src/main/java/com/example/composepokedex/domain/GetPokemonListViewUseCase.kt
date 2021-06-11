8f1813217ed1584f31b03f0df401df2c55b229bb
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-06-11 14:27:35 +0900

    [App] UseCase作成

---
+++ domain/src/main/java/com/example/composepokedex/domain/GetPokemonListViewUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.composepokedex.domain
+
+import com.example.composepokedex.model.model.PokeDexException
+import com.example.composepokedex.model.model.PokemonListView
+import com.example.composepokedex.repository.PokemonListViewRepository
+import javax.inject.Inject
+import com.github.michaelbull.result.Result
+
+interface GetPokemonListViewUseCase {
+    suspend fun execute(): Result<PokemonListView, PokeDexException>
+}
+
+internal class GetPokemonListViewUseCaseImpl @Inject constructor(
+    private val pokemonListViewRepository: PokemonListViewRepository
+): GetPokemonListViewUseCase {
+    override suspend fun execute(): Result<PokemonListView, PokeDexException> {
+        return pokemonListViewRepository.fetchData()
+    }
+}
