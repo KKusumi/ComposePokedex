1b3a7c0b9d5395cdaf71ccc49ae84c92efb8c7b5
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-06-15 10:20:58 +0900

    [App] Dep.kt整理 ・ComposeをAndroidX配下に移動

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -13,6 +13,18 @@ object Dep {
         const val appCompat = "androidx.appcompat:appcompat:1.3.0"
         const val activityCompose = "androidx.activity:activity-compose:1.3.0-beta01"
 
+        object Compose {
+            const val version = "1.0.0-beta08"
+            const val material = "androidx.compose.material:material:$version"
+            const val runtime = "androidx.compose.runtime:runtime-livedata:$version"
+
+            object UI {
+                const val ui = "androidx.compose.ui:ui:$version"
+                const val tooling = "androidx.compose.ui:ui-tooling:$version"
+                const val test = "androidx.compose.ui:ui-test-junit4:$version"
+            }
+        }
+
         object Navigation {
             const val compose = "androidx.navigation:navigation-compose:2.4.0-alpha02"
         }
@@ -36,18 +48,6 @@ object Dep {
             const val version = "2.36"
             const val android = "com.google.dagger:hilt-android:$version"
             const val compiler = "com.google.dagger:hilt-android-compiler:$version"
-        }
-    }
-
-    object Compose {
-        const val version = "1.0.0-beta08"
-        const val material = "androidx.compose.material:material:$version"
-        const val runtime = "androidx.compose.runtime:runtime-livedata:$version"
-
-        object UI {
-            const val ui = "androidx.compose.ui:ui:$version"
-            const val tooling = "androidx.compose.ui:ui-tooling:$version"
-            const val test = "androidx.compose.ui:ui-test-junit4:$version"
         }
     }
 
