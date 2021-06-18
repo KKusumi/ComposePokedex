c3ed315d465c1d2653e5378242376747badf6bf3
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-06-18 15:33:16 +0900

    [Home] レイアウトブラッシュアップ

--- presentation/home/src/main/java/com/example/composepokedex/home/HomeScreen.kt
+++ presentation/home/src/main/java/com/example/composepokedex/home/HomeScreen.kt
@@ -1,21 +1,20 @@
 package com.example.composepokedex.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Surface
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composepokedex.model.model.PokemonListView
 import com.google.accompanist.coil.rememberCoilPainter
@@ -27,8 +26,7 @@ fun HomeScreen(
     val pokemonListView by homeViewModel.pokemonListView.observeAsState(PokemonListView.getEmptyInstance())
     LazyColumn(
         modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
+            .fillMaxWidth(),
     ) {
         items(items = pokemonListView.pokemons) { pokemon ->
             HomeListItem(pokemon = pokemon)
@@ -46,19 +44,48 @@ fun HomeScreenPreview() {
 fun HomeListItem(
     pokemon: PokemonListView.Pokemon
 ) {
-    Row {
-        Surface {
-            Image(
-                modifier = Modifier.padding(16.dp),
-                painter = rememberCoilPainter(
-                    request = pokemon.thumbnailImageUrl
-                ),
-                contentDescription = pokemon.name
+    Row(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .width(48.dp)
+                .height(48.dp),
+            painter = rememberCoilPainter(
+                request = pokemon.thumbnailImageUrl
+            ),
+            contentDescription = pokemon.name
+        )
+        Column(modifier = Modifier.padding(start = 24.dp)) {
+            Text(
+                text = "No.${pokemon.number}",
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 11.sp,
+                lineHeight = 13.sp
             )
+
             Text(
                 text = pokemon.name,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                fontSize = 17.sp,
+                lineHeight = 22.sp,
+                fontWeight = FontWeight.Bold
             )
         }
     }
+    Divider()
+}
+
+@Preview
+@Composable
+fun HomeListItemPreview() {
+    HomeListItem(
+        pokemon = PokemonListView.Pokemon(
+            name = "bulbasour",
+            url = "https://pokeapi.co/api/v2/pokemon/1/"
+        )
+    )
 }
